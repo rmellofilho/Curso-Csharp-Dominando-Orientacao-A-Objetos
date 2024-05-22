dcd14b737c50ab05b2d49e32bcf9b0ebe9af1dfe
Robeto Melo <rmello.filho@hotmail.com>
2024-05-22 17:25:16 -0300

    04 Namespaces

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+﻿using ScreenSound.Modelos;
+
+Banda ira = new Banda("Ira!"); 
+Banda beatles = new("The Beatles");
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
 bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6 });
 bandasRegistradas.Add("The Beatles", new List<int>());
 
