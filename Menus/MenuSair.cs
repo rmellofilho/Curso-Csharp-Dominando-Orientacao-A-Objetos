6d0148aa9ff4ab26b18e1797cc836311b2d06a55
Robeto Melo <rmello.filho@hotmail.com>
2024-05-27 19:45:22 -0300

    atividade

---
+++ Menus/MenuSair.cs
@@ -0,0 +1,11 @@
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuSair : Menu
+{
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.WriteLine("Tchau tchau :)");
+    }
+}
