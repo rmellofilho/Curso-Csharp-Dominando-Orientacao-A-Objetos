2cb42f783dd4ee7184bc799cb1a10e49ff861d73
Robeto Melo <rmello.filho@hotmail.com>
2024-05-27 20:11:12 -0300

    05 Removendo o switch

--- Menus/Menu.cs
+++ Menus/Menu.cs
@@ -1,3 +1,5 @@
+using ScreenSound.Modelos;
+
 namespace ScreenSound.Menus;
 
 internal class Menu
@@ -9,5 +11,10 @@ internal class Menu
         Console.WriteLine(asteriscos);
         Console.WriteLine(titulo);
         Console.WriteLine(asteriscos + "\n");
+    }
+
+    public virtual void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
     }
 }
