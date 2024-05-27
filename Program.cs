eb3408e5810c788a978f08c0313c70c379e0cd93
Robeto Melo <rmello.filho@hotmail.com>
2024-05-27 19:35:48 -0300

    03 Identificando semelhanças

--- Program.cs
+++ Program.cs
@@ -52,11 +52,13 @@ void ExibirOpcoesDoMenu()
             MostrarBandasRegistradas();
             break;
         case 4:
-            AvaliarUmaBanda();
+            MenuAvaliarBanda menu4 = new();
+            menu4.Executar(bandasRegistradas);
+            ExibirOpcoesDoMenu();
             break;
         case 5:
-            MenuExibirDetalhes menu = new MenuExibirDetalhes();
-            menu.Executar(bandasRegistradas);
+            MenuExibirDetalhes menu5 = new MenuExibirDetalhes();
+            menu5.Executar(bandasRegistradas);
             ExibirOpcoesDoMenu();
             break;
         case -1:
@@ -135,34 +137,6 @@ void ExibirTituloDaOpcao(string titulo){
     Console.WriteLine(titulo);
     Console.WriteLine(asteriscos + "\n");
 }
-}
-
-void AvaliarUmaBanda()
-{
-    Console.Clear();
-    ExibirTituloDaOpcao("Avaliar banda");
-    Console.Write("Digite o nome da banda que deseja avaliar: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-        Banda banda = bandasRegistradas[nomeDaBanda];
-        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-        Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-        banda.AdicionarNota(nota);
-        Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
-        Thread.Sleep(2000);
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
-    else
-    {
-        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-        Console.ReadKey();
-        Console.Clear();
-        ExibirOpcoesDoMenu();
-    }
-
 }
 
 ExibirOpcoesDoMenu();
