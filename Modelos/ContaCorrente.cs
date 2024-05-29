e53b03b2ad5699dcf36308faccb472189c0123e9
Robeto Melo <rmello.filho@hotmail.com>
2024-05-29 14:51:18 -0300

    resolvendo

--- Modelos/ContaCorrente.cs
+++ Modelos/ContaCorrente.cs
@@ -3,6 +3,6 @@ class ContaCorrente : ContaBancaria
     private double TaxaManutencao { get; set; }
     public override void Sacar(double valor)
     {
-        base.Sacar(valor);
+        base.Sacar(valor + TaxaManutencao);
     }
 }
--- Modelos/ContaPoupanca.cs
+++ Modelos/ContaPoupanca.cs
@@ -1,4 +1,8 @@
 class ContaPoupanca : ContaBancaria
 {
-
+    private double TaxaManutencao { get; set; }
+    public override void Sacar(double valor)
+    {
+        base.Sacar(valor + TaxaManutencao);
+    }
 }
